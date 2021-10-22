90c1368e8726e1958c1729d2b2c1789aaed71e88
harish <harishkumarvijayarajan@gmail.com>
2021-10-22 21:09:57 +0530

    version 1 update

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import fetch from 'node-fetch'
-let api = "http://api.coinlayer.com/live?access_key=e162a9f47e519469c6657b8390a349b5"
 
-function getPrice() {
+let coinlayerapi = "http://api.coinlayer.com/live?access_key=e162a9f47e519469c6657b8390a349b5"
+
+let coingeckoapi = `https://api.coingecko.com/api/v3/simple/price?ids=Ethereum&vs_currencies=USD&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true`
+
+let coingeckoapilist = "https://api.coingecko.com/api/v3/simple/supported_vs_currencies"
+
+
+function cryptoData() {
 	return new Promise((done, err) => {
 		err('Use One of Our Methods, Please Check API for reference on Methods')
 	})
 }
 
-getPrice.getcoinprice = ((crypto) => {
+cryptoData.getcoinprice = ((crypto) => {
 	return new Promise((done, err) => {
 		if(typeof crypto === 'undefined') {
 			err('You forgot to provide Crypto Currency Name')
 		} else {
-			fetch(api)
+			fetch(coinlayerapi)
 				.then(response => {
 					if (response.status === 200) {
 						return response.json()
@@ -26,4 +32,76 @@ getPrice.getcoinprice = ((crypto) => {
 	})
 })
 
-export default getPrice;
+cryptoData.getcoinvalues = ((price_type) => {
+	return new Promise((done, err) => {
+		if(typeof price_type === 'undefined') {
+			err('You forgot to provide Price Type')
+		} else {
+			fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${price_type}&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
+				.then(response => {
+					if (response.status === 200) {
+						return response.json()
+					} else {
+						err(`Sorry, We couldn't get the Price. Bad response code : ` + response.status)
+					}
+				})
+				.then(json => done(json))
+				.catch(error => console.error(`Sorry, We couldn't get the Price. Error: ` + error))
+		}
+	})
+})
+
+cryptoData.coinprice = ((crypto,currency) => {
+	return new Promise((done, err) => {
+		if(typeof crypto === 'undefined' && typeof currency === 'undefined') {
+			err('You forgot to provide Crypto Currency Name')
+		} else {
+			fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${crypto}&vs_currencies=${currency}&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true`)
+				.then(response => {
+					if (response.status === 200) {
+						return response.json()
+					} else {
+						err(`Sorry, We couldn't get the Price. Bad response code : ` + response.status)
+					}
+				})
+				.then(json => done(json))
+				.catch(error => console.error(`Sorry, We couldn't get the Price. Error: ` + error))
+		}
+	})
+})
+
+cryptoData.coinhistory = ((crypto,date) => {
+	return new Promise((done, err) => {
+		if(typeof crypto === 'undefined' && typeof date === 'undefined') {
+			err('You forgot to provide Crypto Currency Name')
+		} else {
+			fetch(`https://api.coingecko.com/api/v3/coins/${crypto}/history?date=${date}&localization=false`)
+				.then(response => {
+					if (response.status === 200) {
+						return response.json()
+					} else {
+						err(`Sorry, We couldn't get the Price. Bad response code : ` + response.status)
+					}
+				})
+				.then(json => done(json))
+				.catch(error => console.error(`Sorry, We couldn't get the Price. Error: ` + error))
+		}
+	})
+})
+
+cryptoData.coinlist = (() => {
+	return new Promise((done, err) => {
+			fetch(coingeckoapilist)
+				.then(response => {
+					if (response.status === 200) {
+						return response.json()
+					} else {
+						err(`Sorry, We couldn't get the Price. Bad response code : ` + response.status)
+					}
+				})
+				.then(json => done(json))
+				.catch(error => console.error(`Sorry, We couldn't get the List. Error: ` + error))
+	})
+})
+
+export default cryptoData;
--- npm_test.js
+++ npm_test.js
@@ -1,7 +1,32 @@
 import crypto from "./index.js";
 
-crypto.getcoinprice("ETH").then(obj => {  
+crypto.getcoinprice("ETH").then(obj => { 
+    console.log("ETH val: ") 
 	console.log(obj)
+}).catch(err => {
+	console.log(err)
+})
+
+crypto.coinprice("ethereum","USD").then(obj => { 
+    console.log("ETH value from coingecko: ", obj) 
+}).catch(err => {
+	console.log(err)
+})
+
+crypto.coinlist().then(obj => { 
+    console.log("CoinList", obj) 
+}).catch(err => {
+	console.log(err)
+})
+
+crypto.getcoinvalues("usd").then(obj => { 
+    console.log("market value", obj) 
+}).catch(err => {
+	console.log(err)
+})
+
+crypto.coinhistory("ethereum","10-10-2021").then(obj => { 
+    console.log("CoinList", obj) 
 }).catch(err => {
 	console.log(err)
 })
